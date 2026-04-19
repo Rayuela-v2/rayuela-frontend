debfda6b2d23351470c7fbd06387757f3a2197a5
Lucas Matwiejczuk <lucas.matwiejczuk@alu.unq.edu.ar>
2026-04-19 20:29:38 -0300

    fix(frontend): center text in SummaryCard

--- src/components/analytics/SummaryCard.vue
+++ src/components/analytics/SummaryCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card class="mx-auto pa-2" variant="outlined" min-height="100">
+  <v-card class="mx-auto pa-2 text-center" variant="outlined" min-height="100">
     <div class="text-overline mb-1 text-grey-darken-1 text-truncate">{{ title }}</div>
     <div class="text-h5 font-weight-bold">
       {{ formattedValue }}
